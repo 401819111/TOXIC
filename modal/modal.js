ae93576014596fe56e56ff94b894a5cc48e2fe02
401819111 <lx401819111@gmail.com>
2018-04-08 11:22:46 +0800

    test

--- modal/modal.js
+++ modal/modal.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const db = require('./config.js');
 module.exports = {
-    details: function (goods_id, callback) {
-        const sql = "select * from goods,goods_detail where goods.goods_id=goods_detail.goods_id and goods.goods_id=?";
-        db.query(sql, [goods_id], callback);
-    },
+    // details: function (goods_id, callback) {
+    //     const sql = "select * from goods,goods_detail where goods.goods_id=goods_detail.goods_id and goods.goods_id=?";
+    //     db.query(sql, [goods_id], callback);
+    // },
     myInformation:function(userName,userSex,userBirth,userPhone,callback){
         let Name="user_name,";
         let Sex="user_sex,";
